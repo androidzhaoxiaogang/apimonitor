eace8952bf7fb1979310e982288c91a78198bc07
androidzhaoxiaogang <18008021570@189.cn>
2013-12-11 15:56:24 +0800

    add history list;

--- src/com/api/monitor/fragments/HistoryMonitorFragment.java
+++ src/com/api/monitor/fragments/HistoryMonitorFragment.java
@@ -1,43 +1,12 @@
 package com.api.monitor.fragments;
 
-import com.api.monitor.R;
+import com.api.monitor.adapters.ExpandAdapter;
 
-import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 
-public class HistoryMonitorFragment extends Fragment {
+public class HistoryMonitorFragment extends RealtimeMonitorFragment {
 
 	@Override
-	public void onActivityCreated(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
-		super.onActivityCreated(savedInstanceState);
+	public void setAdapter() {
+		mAdapter = new ExpandAdapter(getActivity(), groupData, childrenData, TYPE_HISTORY);
 	}
-
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
-		super.onCreate(savedInstanceState);
-	}
-
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) {
-		return inflater.inflate(R.layout.item_list, null);
-	}
-
-	@Override
-	public void onDestroy() {
-		// TODO Auto-generated method stub
-		super.onDestroy();
-	}
-
-	@Override
-	public void onDestroyView() {
-		// TODO Auto-generated method stub
-		super.onDestroyView();
-	}
-
 }
